63bdec3d7e83a2a895666e003d830df2768d1cb2
Officially-aditya <93206009+Officially-aditya@users.noreply.github.com>
2024-10-02 00:17:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Use CORS middleware
+app.use(cors({
+    origin: 'https://map-application-frontend.vercel.app', // Replace with your frontend URL
+}));
 
 app.get('/route', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'route.json'));
